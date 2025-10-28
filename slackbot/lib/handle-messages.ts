28c4c9395e72da8923e5f05f6e79a556bda642d2
Akai Kaede <genko2@hotmail.com>
2025-10-28 10:09:51 -0700

    refactor handle-messages.ts to improve message handling and update function signature

--- slackbot/lib/handle-messages.ts
+++ slackbot/lib/handle-messages.ts
@@ -1,31 +1,41 @@
-import type { AssistantThreadStartedEvent } from '@slack/web-api';
-import { client } from './slack-utils';
+import type { GenericMessageEvent } from '@slack/web-api';
+import { client, getThread, updateStatusUtil } from "./slack-utils";
+import { generateResponse } from './generate-response';
 
-export async function assistantThreadMessage(
-  event: AssistantThreadStartedEvent,
+export async function handleNewAssistantMessage(
+  event: GenericMessageEvent,
+  botUserId: string,
 ) {
-  const { channel_id, thread_ts } = event.assistant_thread;
-  console.log(`Thread started: ${channel_id} ${thread_ts}`);
-  console.log(JSON.stringify(event));
+  if (
+    event.bot_id ||
+    event.bot_id === botUserId ||
+    event.bot_profile ||
+    !event.thread_ts
+  )
+    return;
+
+  const { thread_ts, channel } = event;
+  const updateStatus = updateStatusUtil(channel, thread_ts);
+  updateStatus('is thinking...');
+
+  const messages = await getThread(channel, thread_ts, botUserId);
+  const result = await generateResponse(messages, updateStatus);
 
   await client.chat.postMessage({
-    channel: channel_id,
+    channel: channel,
     thread_ts: thread_ts,
-    text: "Hello, I'm an AI assistant built with the AI SDK by Vercel!",
-  });
-
-  await client.assistant.threads.setSuggestedPrompts({
-    channel_id: channel_id,
-    thread_ts: thread_ts,
-    prompts: [
+    text: result,
+    unfurl_links: false,
+    blocks: [
       {
-        title: 'Get the weather',
-        message: 'What is the current weather in London?',
-      },
-      {
-        title: 'Get the news',
-        message: 'What is the latest Premier League news from the BBC?',
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: result,
+        },
       },
     ],
   });
+
+  updateStatus('');
 }

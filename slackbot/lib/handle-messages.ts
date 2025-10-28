274c571d1ba8b147142e2277cdce7b96aa0db6ae
Akai Kaede <genko2@hotmail.com>
2025-10-28 10:08:25 -0700

    refactor handle-messages.ts for consistent string quoting and removal of unused code

--- slackbot/lib/handle-messages.ts
+++ slackbot/lib/handle-messages.ts
@@ -1,9 +1,5 @@
-import type {
-  AssistantThreadStartedEvent,
-  GenericMessageEvent,
-} from "@slack/web-api";
-import { client, getThread, updateStatusUtil } from "./slack-utils";
-import { generateResponse } from "./generate-response";
+import type { AssistantThreadStartedEvent } from '@slack/web-api';
+import { client } from './slack-utils';
 
 export async function assistantThreadMessage(
   event: AssistantThreadStartedEvent,
@@ -23,51 +19,13 @@ export async function assistantThreadMessage(
     thread_ts: thread_ts,
     prompts: [
       {
-        title: "Get the weather",
-        message: "What is the current weather in London?",
+        title: 'Get the weather',
+        message: 'What is the current weather in London?',
       },
       {
-        title: "Get the news",
-        message: "What is the latest Premier League news from the BBC?",
+        title: 'Get the news',
+        message: 'What is the latest Premier League news from the BBC?',
       },
     ],
   });
-}
-
-export async function handleNewAssistantMessage(
-  event: GenericMessageEvent,
-  botUserId: string,
-) {
-  if (
-    event.bot_id ||
-    event.bot_id === botUserId ||
-    event.bot_profile ||
-    !event.thread_ts
-  )
-    return;
-
-  const { thread_ts, channel } = event;
-  const updateStatus = updateStatusUtil(channel, thread_ts);
-  updateStatus("is thinking...");
-
-  const messages = await getThread(channel, thread_ts, botUserId);
-  const result = await generateResponse(messages, updateStatus);
-
-  await client.chat.postMessage({
-    channel: channel,
-    thread_ts: thread_ts,
-    text: result,
-    unfurl_links: false,
-    blocks: [
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: result,
-        },
-      },
-    ],
-  });
-
-  updateStatus("");
 }

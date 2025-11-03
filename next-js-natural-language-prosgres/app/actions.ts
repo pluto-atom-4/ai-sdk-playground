b1f09650476ee8847f5f41173762f8fad9c43ec4
Akai Kaede <genko2@hotmail.com>
2025-11-03 10:39:54 -0800

    feat: add chart configuration generation based on SQL query results

--- next-js-natural-language-prosgres/app/actions.ts
+++ next-js-natural-language-prosgres/app/actions.ts
@@ -1,11 +1,10 @@
 "use server";
 
 import { sql } from "@vercel/postgres";
-import { Result } from "@/lib/types";
 import { generateObject } from 'ai';
 import { openai } from '@ai-sdk/openai';
 import { z } from 'zod';
-import { explanationSchema } from '@/lib/types';
+import { Config, configSchema, explanationSchema, Result } from '@/lib/types';
 
 /**import { explanationSchema } from '@/lib/types';
  *
@@ -88,5 +87,53 @@ export const explainQuery = async (input: string, sqlQuery: string) => {
   } catch (e) {
     console.error(e);
     throw new Error('Failed to generate query');
+  }
+};
+
+export const generateChartConfig = async (
+  results: Result[],
+  userQuery: string,
+) => {
+  'use server';
+
+  try {
+    const { object: config } = await generateObject({
+      model: openai('gpt-4o'),
+      system: 'You are a data visualization expert.',
+      prompt: `Given the following data from a SQL query result, generate the chart config that best visualises the data and answers the users query.
+      For multiple groups use multi-lines.
+
+      Here is an example complete config:
+      export const chartConfig = {
+        type: "pie",
+        xKey: "month",
+        yKeys: ["sales", "profit", "expenses"],
+        colors: {
+          sales: "#4CAF50",    // Green for sales
+          profit: "#2196F3",   // Blue for profit
+          expenses: "#F44336"  // Red for expenses
+        },
+        legend: true
+      }
+
+      User Query:
+      ${userQuery}
+
+      Data:
+      ${JSON.stringify(results, null, 2)}`,
+      schema: configSchema,
+    });
+
+    // Override with shadcn theme colors
+    const colors: Record<string, string> = {};
+    config.yKeys.forEach((key, index) => {
+      colors[key] = `hsl(var(--chart-${index + 1}))`;
+    });
+
+    const updatedConfig = { ...config, colors };
+    return { config: updatedConfig };
+  } catch (e) {
+    console.error(e);
+    throw new Error('Failed to generate chart suggestion');
   }
 };

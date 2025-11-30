d0bd09f563a66259c430ac27b7540251326bb466
Akai Kaede <genko2@hotmail.com>
2025-11-30 08:22:02 -0800

    feat: init stream text with image prompt

---
+++ next-js-stream-text-with-image-prompt/app/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useCompletion } from "@ai-sdk/react";
+
+export default function Page() {
+  const {completion, complete} = useCompletion({
+    api: "/api/completion",
+  });
+
+  return (
+    <div>
+      <div
+        onClick={async () => {
+          await complete("Why is the sky blue?");
+        }}>
+        Generate
+
+      </div>
+      {completion}
+    </div>
+  );
+}
